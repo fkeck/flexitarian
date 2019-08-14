0d3321dc6d01f2f028964ab3301842b86122c51b
fkeck <francois.keck@gmail.com>
2019-08-14 12:07:54 +0200

    Add keep order option in spread_cdm

--- R/functions.R
+++ R/functions.R
@@ -9,16 +9,17 @@
 #' @return a dataframe in wide format (typical sites x species community data matrix).
 #' @export
 #' @examples
-#' x <- matrix(rpois(100, 10), nrow = 10)
+#' x <- data.frame(matrix(rpois(100, 10), nrow = 10))
 #' rownames(x) <- paste("Site", 1:10)
 #' colnames(x) <- paste("Species", LETTERS[1:10])
 #' x
 #' x_tidy <- tidy_cdm(x)
 #' x_tidy
-#' y <- spread_cdm(x_tidy, SITE, TAXON, COUNT)
-#' y
-#'
-spread_cdm <- function(x, row.names, col.names, counts) {
+#' x_df <- spread_cdm(x_tidy, SITE, TAXON, COUNT)
+#' x_df
+#' x_df <- spread_cdm(x_tidy, SITE, TAXON, COUNT, keep.order = TRUE)
+#' all.equal(x, x_df)
+spread_cdm <- function(x, row.names, col.names, counts, keep.order = FALSE) {
 
   quo_counts <- dplyr::enquo(counts)
   quo_col.names <- dplyr::enquo(col.names)
@@ -30,6 +31,12 @@ spread_cdm <- function(x, row.names, col.names, counts) {
   res <- dplyr::select(res, -!!quo_row.names)
   res <- as.data.frame(res)
   rownames(res) <- row_names
+
+    if(keep.order){
+  res <- res[unique(deframe(select(x, !!quo_row.names))),
+             unique(deframe(select(x, !!quo_col.names)))]
+    }
+
   return(res)
 }
 
